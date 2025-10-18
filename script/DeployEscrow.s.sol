8073ca7d1b5a4b8e4439cdc9748048917e3f094b
Ikem Peter <ikempeter2020@gmail.com>
2025-10-18 19:54:08 +0100

    feat: deploy escrow registry contract with transaction hash verification

--- script/DeployEscrow.s.sol
+++ script/DeployEscrow.s.sol
@@ -7,7 +7,7 @@ import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy
 
 contract DeployEscrow is Script {
     function run() external returns (address impl, address proxy) {
-        address deployer = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
+        address deployer = vm.rememberKey(uint256(vm.envBytes32("PRIVATE_KEY")));
         address operator = vm.envOr("ESCROW_OPERATOR", address(0));
 
         vm.startBroadcast(deployer);
